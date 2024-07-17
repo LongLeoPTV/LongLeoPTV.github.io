83989691cb6c493f5034eced5645d092129c6ebf
LongLeoPTV <longptv@gmail.com>
2024-07-17 23:19:09 +0700

    homework_9

---
+++ 16th_project_homework9_countdown/js/countdown.js
@@ -0,0 +1,29 @@
+var min = 9;
+var sec = 60;
+
+var cdmin = setInterval(minutes, 60000);
+
+function minutes() {
+  min--;
+  if (min < 0) {
+    clearInterval(cdmin);
+    alert("You are out of time for this task");
+  }
+}
+
+var cdsec = setInterval(seconds, 1000);
+
+function seconds() {
+  sec--;
+  if (sec < 0 && min >= 0) {
+    sec = 59;
+  } else if (sec === 0 && min < 0) {
+    clearInterval(cdsec);
+  }
+
+  updateTime();
+}
+
+function updateTime() {
+  console.log(`Bạn còn ${min} : ${sec}`);
+}
